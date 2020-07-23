9b1038c14026b7b70fa6b1d42a886d6fd68bb410
SkybenSK <it.tounsia@gmail.com>
2020-07-23 16:14:10 +0100

    boot

--- src/App.js
+++ src/App.js
@@ -6,18 +6,32 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CONTACT</h1>
+        <form action="#" method="post">
+          <div class="form-group">
+            <input type="text" name="first-name" id="first-name" placeholder="First name" class="form-control"></input>
+          </div>
+          <div class="form-group">
+            <input type="text" name="first-name" id="first-name" placeholder="First name" class="form-control"></input>
+          </div>
+          <div class="form-group">
+            <input type="text" name="first-name" id="first-name" placeholder="First name" class="form-control"></input>
+          </div>
+          <div class="form-group">
+            <input type="email" name="email" id="email" placeholder="Email" class="form-control"></input>
+          </div>
+          <div class="form-group">
+            <input type="tel" name="tel" id="tel" placeholder="Tel" minlength="6" maxlength="10" class="form-control"></input>
+          </div>
+          <div class="form-group">
+            <label for="message"></label>
+            <textarea name="message" id="message" cols="30" rows="10" class="form-control"></textarea>
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-dark">Send</button>
+            <button type="reset" class="btn btn-light">Reset</button>
+          </div>
+        </form>
       </header>
     </div>
   );
